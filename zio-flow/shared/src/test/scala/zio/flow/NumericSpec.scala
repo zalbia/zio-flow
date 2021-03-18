757543a1439f73494b5805c5cae5dac3c40858c6
ashprakasan <prakasanaiswarya@gmail.com>
2021-03-19 02:55:53 +0530

    Math operators and tests.

--- zio-flow/shared/src/test/scala/zio/flow/NumericSpec.scala
+++ zio-flow/shared/src/test/scala/zio/flow/NumericSpec.scala
@@ -5,11 +5,30 @@ import zio.test.{ DefaultRunnableSpec, ZSpec, _ }
 
 object RemoteNumericSpec extends DefaultRunnableSpec {
 
-  val suite1: Spec[Any,TestFailure[Nothing],TestSuccess] = suite("RemoteIntAddition")(test("Test addition of Remote") {
-    val result = (Remote(1) + Remote(2)).eval
-    assert(result)(isRight(equalTo(3)))
-  })
+  val suite1 = suite("Test addition of Remote Int111")(
+    test("Basic addition.") {
+      assert((Remote(1)+ Remote(2)).eval)(equalTo(Remote(3).eval))
+    },
+    testM("Associativity of addition.") {
+      check(Gen.anyInt, Gen.anyInt, Gen.anyInt) { (int1, int2, int3) =>
+        val remoteInt1 = Remote(int1)
+        val remoteInt2 = Remote(int2)
+        val remoteInt3 = Remote(int3)
+        val r1         = ((remoteInt1 + remoteInt2) + remoteInt3).eval
+        val r2         = (remoteInt1 + (remoteInt2 + remoteInt3)).eval
+        assert(r1)(equalTo(r2))
+      }
+    },
+    testM("Commutativity of addition.") {
+      check(Gen.anyInt, Gen.anyInt) { (int1, int2) =>
+        val remoteInt1 = Remote(int1)
+        val remoteInt2 = Remote(int2)
+        val r1         = (remoteInt1 + remoteInt2).eval
+        val r2         = (remoteInt2 + remoteInt1).eval
+        assert(r1)(equalTo(r2))
+      }
+    })
 
-  //val suite2 = suite("Associativity of addition")(test("Associativity of addition"){
-  override def spec: ZSpec[_root_.zio.test.environment.TestEnvironment, Any] = suite("All tests")(suite1)
+  override def spec: ZSpec[_root_.zio.test.environment.TestEnvironment, Any] =
+    suite("All tests")(suite1)
 }
